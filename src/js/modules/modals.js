3ab93dc93b542e28336e95b66c5aa00354749bd5
Elena <ebe2017@yandex.ru>
2024-05-04 19:25:16 +0400

    modal gift

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,5 +1,6 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+    let btnPressed = false;
+    function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
               close = document.querySelector(closeSelector),
@@ -11,6 +12,12 @@ const modals = () => {
                 if (e.target) {
                     e.preventDefault();
                 }
+
+                btnPressed = true;
+
+                if (destroy) {
+                    item.remove();
+                }
         
                 let shouldOpenModal = true;
 
@@ -18,6 +25,7 @@ const modals = () => {
                 if (shouldOpenModal) {
                     windows.forEach(item => {
                         item.style.display = 'none';
+                        item.classList.add('animated', 'fadeIn')
                     });
         
                     modal.style.display = 'block';
@@ -35,7 +43,7 @@ const modals = () => {
         });
 
         modal.addEventListener('click', (e) => {
-            if (e.target === modal && closeClickOverlay) {
+            if (e.target === modal) {
                 windows.forEach(item => {
                     item.style.display = 'none';
                 });
@@ -63,9 +71,19 @@ const modals = () => {
         }, time);
     }
 
+    function openByScroll(selector) {
+        window.addEventListener('scroll', () => {
+            if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= 
+                document.documentElement.scrollHeight)) 
+                document.querySelector(selector).click();
+        });
+    }
+
     bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
     bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-    showModalByTime('.popup-consultation', 5000);
+    bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+    openByScroll('.fixed-gift');
+    // showModalByTime('.popup-consultation', 5000);
 }
 
 export default modals;
